3f7712825411b4e86a7edf9a644807a63eefd493
Alice Abarca <aliceabarca@otmail.com>
2023-08-24 18:19:46 -0600

    Add movies components with functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import movieData from './movieData';
+import Card from './components/cards/Cards'
+import { useState } from 'react';
+import Movies from './components/movies/Movies';
 
 function App() {
+  const [movies, setMovies] = useState(movieData)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movies movies={movies}/>
     </div>
   );
 }
