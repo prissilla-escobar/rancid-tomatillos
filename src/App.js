b7766bd6bf18962811be9526fd834bf96d6bdf49
Alice Abarca <aliceabarca@otmail.com>
2023-08-26 13:36:20 -0600

    Add use effect for our api fetch call

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
-import movieData from './movieData';
+// import movieData from './movieData';
 import Card from './components/cards/Cards'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Movies from './components/movies/Movies';
 import SelectedMovie from './components/selectedMovie/SelectedMovie';
+import {getAllMovies, getSelectedMovieData, getVideoData} from './api';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-  const [movies, setMovies] = useState(movieData)
+  const [movies, setMovies] = useState([])
   const [selectedMovie, setSelectedMovie] = useState(false)
+  const [trailerKey, setTrailerKey] = useState('')
+
+  useEffect(() => {
+    getAllMovies() 
+      .then(data => {
+        return setMovies(data.movies)
+      })
+    }, [])
+
+  useEffect(() => {
+    getVideoData(id)
+    .then(vidData => setTrailerKey(vidData))
+  })
 
   function showSelectedMovie(id) {
-    const singleMovie = movieData.movies.find(movie => movie.id === id)
+    const singleMovie = movies.find(movie => movie.id === id)
     setSelectedMovie(singleMovie)
+    getSelectedMovieData(id).then(data => setSelectedMovie(data.movie))
+
   }
   
   function showMovies() {
