ac8968817e41e14c3c4bfbe81214c1a05bccb11b
Prissilla Escobar <prissilla988@gmail.com>
2023-08-31 18:37:01 -0600

    Add Link to home page

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -1,10 +1,13 @@
 import './Header.css'
 import logo from '../../images/Rancid Logo light red.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header>
-            <img className="rancid-logo" src={logo} alt="a movie theater sign that says rancid tomatillos" />
+            <Link to={`/`}>
+                <img className="rancid-logo" src={logo} alt="a movie theater sign that says rancid tomatillos" />
+            </Link>
         </header>
     )
 }
