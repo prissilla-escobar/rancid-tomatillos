dac26abc6e47f2821719f58b733e51b447de29af
Prissilla Escobar <prissilla988@gmail.com>
2023-08-31 18:40:05 -0600

    Add Link to home page for back button and add ? for all methods that are using fetched data

--- src/components/selectedMovie/SelectedMovieCard.js
+++ src/components/selectedMovie/SelectedMovieCard.js
@@ -2,24 +2,27 @@
 import './SelectedMovie.css'
 import PropTypes from 'prop-types'
 import rewindImage from '../../images/rewind-button (1).png'
+import { Link } from 'react-router-dom'
 
 function SelectedMovieCard(props) {
-
     const dayjs = require('dayjs')
+
     return (
       <div className='selected-movie-card'>
         <div className='selected-movie-container'>
-          <img className='back-button' alt='back button' src={rewindImage} onClick={() => {props.showMovies()}}></img>
+          <Link to={`/`}>
+            <img className='back-button' alt='back button' src={rewindImage} onClick={() => {props.showMovies()}}></img>
+          </Link>
           <div className='selected-movie-poster-container'>
             <img src={props.poster_path} className="selected-movie-poster" alt={`movie poster for the movie titled ${props.title}`}></img>
             <div className='movie-info'>
               <h1 className='movie-title'>{props.title}</h1>
               <div className='release-year'>
-                <h2>({dayjs(props.release_date).format('YYYY')}), {props.runtime}min</h2>
+                <h2>({dayjs(props.release_date)?.format('YYYY')}), {props.runtime}min</h2>
               </div>
               <h3 className='rating'>
-                Average Rating: {props.average_rating.toFixed(1) * 10}% &#127813;<br />
-                {props.genres.join('/')}
+                Average Rating: {props.average_rating?.toFixed(1) * 10}% &#127813;<br />
+                {props.genres?.join('/')}
               </h3>
               <p className='overview'>
                 {props.overview}
@@ -35,11 +38,11 @@ function SelectedMovieCard(props) {
   export default SelectedMovieCard;
 
   SelectedMovieCard.propTypes = {
-    poster_path: PropTypes.string.isRequired,
-    backdrop_path: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    release_date: PropTypes.string.isRequired,
-    overview: PropTypes.string.isRequired,
-    trailerKey: PropTypes.string.isRequired,
-    showMovies: PropTypes.func.isRequired
+    poster_path: PropTypes.string,
+    backdrop_path: PropTypes.string,
+    title: PropTypes.string,
+    release_date: PropTypes.string,
+    overview: PropTypes.string,
+    trailerKey: PropTypes.string,
+    showMovies: PropTypes.func
   } 
