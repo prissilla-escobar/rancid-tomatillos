82ba819f5d83ac899a46e43e7c17ddcd2edac28d
Alice Abarca <aliceabarca@otmail.com>
2023-08-31 19:06:40 -0600

    Fix error

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,7 +2,6 @@ import './Cards.css'
 import PropTypes from 'prop-types'
 
 function Card(props) {
-  console.log(props)
   return (
     <div className="card" onClick={() => props.showSelectedMovie(props.id)}>
       <img src={props.poster_path} alt={`movie poster for the movie titled ${props.title}`} className="movie-poster"></img>
