e6f84380883288b00a8174aaaf6cc706db1f2793
Prissilla Escobar <prissilla988@gmail.com>
2023-09-02 13:00:00 -0600

    Remove the function that we no longer need to pass through since we added useState and useEffect into SelectedMOvieCard.

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function Card(props) {
 
   return (
-    <div className="card" onClick={() => props.showSelectedMovie(props.id)}>
+    <div className="card" >
       <img src={props.poster_path} alt={`movie poster for the movie titled ${props.title}`} className="movie-poster"></img>
     </div>
   )
@@ -15,5 +15,4 @@ export default Card;
 Card.propTypes = {
   id: PropTypes.number.isRequired,
   poster_path: PropTypes.string.isRequired,
-  showSelectedMovie: PropTypes.func.isRequired
 }
