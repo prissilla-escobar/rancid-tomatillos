4d870a3ff47d222840c3c6c1f43264219da6f5ad
Prissilla Escobar <prissilla988@gmail.com>
2023-09-02 13:05:27 -0600

    Create useStates for selectedMovie and trailerKey, remove props that we were passing through and update the return to the useStates rather than the props. Add useEffect for fetching the data. This fixes our error of the selected movie card breaking on refresh

--- src/components/selectedMovie/SelectedMovieCard.js
+++ src/components/selectedMovie/SelectedMovieCard.js
@@ -1,48 +1,55 @@
-
 import './SelectedMovie.css'
-import PropTypes from 'prop-types'
 import rewindImage from '../../images/rewind-button (1).png'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { getSelectedMovieData, getVideoData } from '../../api';
 
-function SelectedMovieCard(props) {
+function SelectedMovieCard({setServerError}) {
     const dayjs = require('dayjs')
+    const {movieId} = useParams()
+    const [selectedMovie, setSelectedMovie] = useState(false)
+    const [trailerKey, setTrailerKey] = useState('')
+
+    useEffect( () => {  
+      getSelectedMovieData(movieId)
+        .then(data => setSelectedMovie(data.movie))
+        .catch(error => setServerError({hasError: true, message: `${error.message}`}))
+      }, [])
+  
+      useEffect( () => {  
+        getVideoData(movieId)
+          .then(key => setTrailerKey(key))
+          .catch(e => setTrailerKey(''))
+      }, [])
 
     return (
-      <div className='selected-movie-card'>
-        <div className='selected-movie-container'>
-          <Link to={`/`}>
-            <img className='back-button' alt='back button' src={rewindImage} onClick={() => {props.showMovies()}}></img>
-          </Link>
-          <div className='selected-movie-poster-container'>
-            <img src={props.poster_path} className="selected-movie-poster" alt={`movie poster for the movie titled ${props.title}`}></img>
-            <div className='movie-info'>
-              <h1 className='movie-title'>{props.title}</h1>
-              <div className='release-year'>
-                <h2>({dayjs(props.release_date)?.format('YYYY')}), {props.runtime}min</h2>
+      <div className='single-movie-container' style={{backgroundImage:`url(${selectedMovie.backdrop_path}`,   backgroundSize:'cover'}}>
+        <div className='selected-movie-card'>
+          <div className='selected-movie-container'>
+            <Link to={`/`}>
+              <img className='back-button' alt='back button' src={rewindImage}></img>
+            </Link>
+            <div className='selected-movie-poster-container'>
+              <img src={selectedMovie.poster_path} className="selected-movie-poster" alt={`movie poster for the movie titled ${selectedMovie.title}`}></img>
+              <div className='movie-info'>
+                <h1 className='movie-title'>{selectedMovie.title}</h1>
+                <div className='release-year'>
+                  <h2>({dayjs(selectedMovie.release_date)?.format('YYYY')}), {selectedMovie.runtime}min</h2>
+                </div>
+                <h3 className='rating'>
+                  Average Rating: {selectedMovie.average_rating?.toFixed(1) * 10}% &#127813;<br />
+                  {selectedMovie.genres?.join('/')}
+                </h3>
+                <p className='overview'>
+                  {selectedMovie.overview}
+                </p>
               </div>
-              <h3 className='rating'>
-                Average Rating: {props.average_rating?.toFixed(1) * 10}% &#127813;<br />
-                {props.genres?.join('/')}
-              </h3>
-              <p className='overview'>
-                {props.overview}
-              </p>
             </div>
+            <iframe className='trailer' title={`Embedded youtube trailer for the movie titled ${selectedMovie.title}`} width='560' height='315' src= {`https://www.youtube-nocookie.com/embed/${trailerKey}`}></iframe>
           </div>
-          <iframe className='trailer' title={`Embedded youtube trailer for the movie titled ${props.title}`} width='560' height='315' src= {`https://www.youtube.com/embed/${props.trailerKey}`}></iframe>
         </div>
       </div>
     )
   }
   
   export default SelectedMovieCard;
-
-  SelectedMovieCard.propTypes = {
-    poster_path: PropTypes.string,
-    backdrop_path: PropTypes.string,
-    title: PropTypes.string,
-    release_date: PropTypes.string,
-    overview: PropTypes.string,
-    trailerKey: PropTypes.string,
-    showMovies: PropTypes.func
-  } 
